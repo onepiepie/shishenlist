a37b9a8b0d952c9de70727cf473f58e2a9f5dbe8
chengengjie <1106230916@qq.com>
2021-03-22 18:29:51 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ class Story extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("https://yys.clmystes.tech/api/get_hero_story?heroid=" + this.props.heroid).then((response) => {
+    axios.get("https://yys.clmystes.tech:3300/api/get_hero_story?heroid=" + this.props.heroid).then((response) => {
       this.setState({
         story: response.data
       })
@@ -48,7 +48,7 @@ class Story extends React.Component {
 
   componentDidUpdate(prevprops,prevstate) {
     if (this.props !== prevprops) {
-      axios.get("https://yys.clmystes.tech/api/get_hero_story?heroid=" + this.props.heroid).then((response) => {
+      axios.get("https://yys.clmystes.tech:3300/api/get_hero_story?heroid=" + this.props.heroid).then((response) => {
         this.setState({
           story: response.data
         })
@@ -134,7 +134,7 @@ class Attr extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("https://yys.clmystes.tech/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=0&level=1&star=2").then((response) => {
+    axios.get("https://yys.clmystes.tech:3300/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=0&level=1&star=2").then((response) => {
       this.setState({
         attr_awake_before: response.data.data
       })
@@ -143,7 +143,7 @@ class Attr extends React.Component {
     })
 
     if (this.props.level !== "SP"){
-      axios.get("https://yys.clmystes.tech/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=1&level=1&star=2").then((response) => {
+      axios.get("https://yys.clmystes.tech:3300/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=1&level=1&star=2").then((response) => {
         this.setState({
           attr_awake_after: response.data.data
         })
@@ -160,7 +160,7 @@ class Attr extends React.Component {
   componentDidUpdate(prevprops,prevstate) {
     /** 第一个参数是上一次的props，第二个参数是上一次的state */
     if (this.props !== prevprops) {
-      axios.get("https://yys.clmystes.tech/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=0&level=1&star=2").then((response) => {
+      axios.get("https://yys.clmystes.tech:3300/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=0&level=1&star=2").then((response) => {
         this.setState({
           attr_awake_before: response.data.data
         })
@@ -169,7 +169,7 @@ class Attr extends React.Component {
       })
   
       if (this.props.level !== "SP"){
-        axios.get("https://yys.clmystes.tech/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=1&level=1&star=2").then((response) => {
+        axios.get("https://yys.clmystes.tech:3300/api/get_hero_attr?heroid="+ this.props.heroid+"&awake=1&level=1&star=2").then((response) => {
           this.setState({
             attr_awake_after: response.data.data
           })
@@ -330,7 +330,7 @@ class TopBar extends React.Component {
       })(document,window);
 
     /** 调用api接口获取数据 */
-    axios.get("https://yys.clmystes.tech/api02/pc/zt/20161108171335/js/app/all_shishen.json?v69").then((response) => {
+    axios.get("https://yys.clmystes.tech:3300/api02/pc/zt/20161108171335/js/app/all_shishen.json?v69").then((response) => {
       this.setState({
         heroidlist: response.data
       });
